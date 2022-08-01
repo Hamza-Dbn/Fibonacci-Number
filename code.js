59e85ffbb2aa988b04dc46b3f741905b48b19ba2
Hamza Darbani <76852492+Hamza-Dbn@users.noreply.github.com>
2022-08-01 20:40:44 +0200

    Create code.js

---
+++ code.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n, memo = {}) {
+    if (n === 0 || n === 1) {
+        return n;
+    }
+    
+    if(!memo[n]) {
+        memo[n] = fib(n-2, memo) + fib (n-1,memo)
+    }
+    
+    return memo[n];
+};
